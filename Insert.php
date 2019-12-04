e30acc1e2a1ed7802ac4893c61c65c6e44f0331c
JadenKandel <58280867+JadenKandel@users.noreply.github.com>
2019-12-04 01:34:27 -0500

    Unsure if it works yet due to php files not working
    Please verify this works ASAP

--- Insert.php
+++ Insert.php
@@ -1,1 +1,42 @@
-WIP
+<!DOCTYPE html>
+<html>
+
+   <head>
+      <title>Add New item into the inventory</title>
+   </head>
+
+<?php
+	  
+	require("queryfunx.php"); 
+	require("dbconnect.php");            
+        
+  $storenum = $_POST['storenum'];
+  $SKU = $_POST['SKU'];
+	$no_item = $_POST['no_item'];
+			
+	echo " <br> Inventory before adding to it<br>";
+	showInventory($conn, $store);
+   
+    $sql = "INSERT INTO Inventory "."(storenum,SKU, no_item) "."VALUES ".
+               "('$storenum','$SKU','$no_item')";
+            
+    $retval = mysqli_query($conn, $sql); // procedural execution of query
+         
+    if(! $retval ) {
+        die('Could not enter data: ' . mysqli_error($conn));
+    }
+         
+    echo "Entered data successfully\n";
+			
+	echo " <br>  Inventory after adding to it<br>";
+	showInventory($conn, $store);
+			
+    mysqli_close($conn);    
+
+?>
+   <hr width="50">
+<a href="Frontpage.html" style="color:red;font-weight:bold;">Home</a>
+<hr width="50">
+   </div>
+</body>
+</html>
